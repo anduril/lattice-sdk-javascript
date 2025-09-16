6de29005ca9ed19f14d0192804a447fe464df494
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-16 18:20:50 +0000

    SDK regeneration

--- src/Client.ts
+++ src/Client.ts
@@ -26,6 +26,8 @@ export declare namespace LatticeClient {
         maxRetries?: number;
         /** A hook to abort the request. */
         abortSignal?: AbortSignal;
+        /** Additional query string parameters to include in the request. */
+        queryParams?: Record<string, unknown>;
         /** Additional headers to include in the request. */
         headers?: Record<string, string | core.Supplier<string | undefined> | undefined>;
     }
@@ -44,8 +46,8 @@ export class LatticeClient {
                 {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "@anduril-industries/lattice-sdk",
-                    "X-Fern-SDK-Version": "2.2.0",
-                    "User-Agent": "@anduril-industries/lattice-sdk/2.2.0",
+                    "X-Fern-SDK-Version": "2.2.1",
+                    "User-Agent": "@anduril-industries/lattice-sdk/2.2.1",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                 },
